0e8e262f6004fbe9c3c4168755d3fb165c5f8524
Alper Donmez <alper.donmez490@gmail.com>
2018-10-12 17:35:58 -0400

    redo sequelize setup

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
+
 const path = require('path');
 const express = require('express');
 const compress = require('compression');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const responseTime = require('response-time');
-const { Client } = require('pg');
 const Sequelize = require('sequelize');
 
 const app = express();
@@ -17,7 +18,40 @@ app.use(compress());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+const User = sequelize.define('user', {
+    firstName: {
+        type: Sequelize.STRING
+    },
+    lastName: {
+        type: Sequelize.STRING
+    }
+});
+
+// force: true will drop the table if it already exists
+User.sync({ force: true }).then(() => {
+    // Table created
+    return User.create({
+        firstName: 'John',
+        lastName: 'Hancock'
+    });
+});
 
 app.listen(port, () => {
     console.log('server started on port: ' + port);
